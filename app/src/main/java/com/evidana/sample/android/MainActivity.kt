6aa7d6f4776fbe1f7cbfc1c7e33e2a6cf624c378
Eric Vidana <eric.vidana@gmail.com>
2021-07-15 20:11:51 -0400

    GTM
    Adding google tag manager and basic ecommerce flow.

--- app/src/main/java/com/evidana/sample/android/MainActivity.kt
+++ app/src/main/java/com/evidana/sample/android/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.evidana.sample.android
 
 import android.os.Bundle
-import android.view.MotionEvent
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.analytics.ktx.logEvent
@@ -17,10 +16,38 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
 
+    data class Product(
+            val name: String,
+            val sku: String,
+            val price: Double,
+            val quantity: Int,
+            val currency: String
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        // initial products
+
+        val guitar = Product("guitar 1", "SKU_123", 199.99, 1, "USD")
+        val cymbal = Product("cymbal 1", "SKU_124", 125.99, 1, "USD")
+
+        var guitarBundle = Bundle()
+        guitarBundle.putString(FirebaseAnalytics.Param.ITEM_ID, guitar.name)
+        guitarBundle.putDouble(FirebaseAnalytics.Param.PRICE, guitar.price)
+        guitarBundle.putInt(FirebaseAnalytics.Param.QUANTITY, guitar.quantity)
+        guitarBundle.putString(FirebaseAnalytics.Param.CURRENCY, guitar.currency)
+
+        var cymbalBundle = Bundle()
+        cymbalBundle.putString(FirebaseAnalytics.Param.ITEM_ID, cymbal.name)
+        cymbalBundle.putDouble(FirebaseAnalytics.Param.PRICE, cymbal.price)
+        cymbalBundle.putInt(FirebaseAnalytics.Param.QUANTITY, cymbal.quantity)
+        cymbalBundle.putString(FirebaseAnalytics.Param.CURRENCY, cymbal.currency)
+
+        var cartValue = guitar.price + cymbal.price
+        var transactionID = (0..10).random()
 
         firebaseAnalytics = Firebase.analytics
 
@@ -36,5 +63,51 @@ class MainActivity : AppCompatActivity() {
             param("image_name", "test")
             param("full_text", "test1")
         }
+
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, "Home Screen")
+        }
+
+        var viewItemBundle = Bundle()
+        viewItemBundle.putString(FirebaseAnalytics.Param.CURRENCY, "USD")
+        viewItemBundle.putDouble(FirebaseAnalytics.Param.VALUE, cartValue)
+        viewItemBundle.putParcelableArrayList(FirebaseAnalytics.Param.ITEMS, arrayListOf(guitarBundle, cymbalBundle))
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM, viewItemBundle)
+
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.ADD_TO_CART, viewItemBundle)
+
+        // remember that begin_checkout always sets checkstep of 1
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.BEGIN_CHECKOUT, viewItemBundle)
+
+        // dev requirements for mapping checkout_step2 to the actual in app event is important
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.CHECKOUT_PROGRESS) {
+            param(FirebaseAnalytics.Param.CURRENCY, "USD")
+            param(FirebaseAnalytics.Param.VALUE, cartValue)
+            param(FirebaseAnalytics.Param.ITEMS, arrayOf(guitarBundle, cymbalBundle))
+            param(FirebaseAnalytics.Param.CHECKOUT_STEP, 2)
+        }
+        // this is just for ga4
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.ADD_SHIPPING_INFO, viewItemBundle)
+
+
+        // dev requirements for mapping checkout_step2 to the actual in app event is important
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.CHECKOUT_PROGRESS) {
+            param(FirebaseAnalytics.Param.CURRENCY, "USD")
+            param(FirebaseAnalytics.Param.VALUE, cartValue)
+            param(FirebaseAnalytics.Param.ITEMS, arrayOf(guitarBundle, cymbalBundle))
+            param(FirebaseAnalytics.Param.CHECKOUT_STEP, 3)
+        }
+        // this is just for ga4
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.ADD_PAYMENT_INFO, viewItemBundle)
+
+        // the event HAS TO BE ecommerce_purchase for ga360 reports to work
+        // ok that it is deprecated
+        // configure this one for GTM
+        // add a rule to drop value
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.ECOMMERCE_PURCHASE, viewItemBundle)
+
+        // the event HAS TO BE purchase for ga4 to work
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.PURCHASE, viewItemBundle)
+
     }
 }
